99a6e971a5cf50448e7946497a0060cbcabc63e5
SashaNotcoiner <shahmatandrey@gmail.com>
2025-04-02 11:46:29 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
-// Конфигурация (замените значения своими!)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBk...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-bucket.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcde..."
+  apiKey: "AIzaSyCBIuy2a6ywwl2QJresRxG_c_FDgY7WgZg",
+  authDomain: "tfyh7-bf405.firebaseapp.com",
+  projectId: "tfyh7-bf405",
+  storageBucket: "tfyh7-bf405.firebasestorage.app",
+  messagingSenderId: "537695747687",
+  appId: "1:537695747687:web:a014bc543d5fa5f70a5ec2",
+  measurementId: "G-P6YV8L4PRR"
 };
 
-// Инициализация Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
